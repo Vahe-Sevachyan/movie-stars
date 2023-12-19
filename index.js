6762a4a2f211d57d38fcdc152bbcd429ad49d998
Vahe Sevachyan <vscode13@gmail.com>
2023-12-18 22:34:38 -0800

    testing async function

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-console.log('Hi there!');
+const fetchData = async () => {
+  const response = await axios.get("http://www.omdbapi.com/", {
+    params: {
+      apikey: "3e28670",
+      s: "avengers",
+    },
+  });
+  console.log(response.data);
+};
+// 3e28670
+// http://www.omdbapi.com/?i=tt3896198&apikey=3e28670
+fetchData();
+const willMasterJS = new Promise((resolve, reject) => {
+  resolve();
+  reject();
+});
+
+const myObject = {
+  name: "vahe",
+  age: 35,
+  location: "unknown",
+};
+// console.log(myObject.name);
+// console.log(myObject.age);
