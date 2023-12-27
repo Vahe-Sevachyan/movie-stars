abc63b57c5c60bc3d560c02c02fda31ca27501c5
Vahe Sevachyan <vscode13@gmail.com>
2023-12-26 19:43:32 -0800

    updated for of loop

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ root.innerHTML = `
 <input class="input">
 <div class="dropdown">
 <div class="dropdown-menu">
-  <div class="dropdown-content"></div>
+  <div class="dropdown-content results"></div>
 </div>
 </div>
 `;
@@ -24,15 +24,18 @@ root.innerHTML = `
 const input = document.querySelector("input");
 const dropdown = document.querySelector(".dropdown");
 const resultsWrapper = document.querySelector(".results");
+
 const onInput = async (event) => {
   const movies = await fetchData(event.target.value);
+  dropdown.classList.add("is-active");
   for (let movie of movies) {
-    const div = document.createElement("div");
-    div.innerHTML = `
+    const option = document.createElement("a");
+    option.classList.add("dropdown-item");
+    option.innerHTML = `
     <img src="${movie.Poster}"/>
-    <h1>${movie.Title}</h1>
+    ${movie.Title}
     `;
-    document.querySelector("#target").appendChild(div);
+    resultsWrapper.appendChild(option);
   }
 };
 input.addEventListener("input", deBounce(onInput, 500));
